baefc16e90bb5175667000689eaca1fc512505d7
Emerson Barbosa Gonçalves <emerson_b.goncalves@outlook.com>
2024-03-05 11:36:55 -0300

    melhorias visuais e classe de projetos

---
+++ assets/carousel.js
@@ -0,0 +1,67 @@
+const imgs = document.getElementById("imgCarousel");
+const botoesCarousel = document.getElementsByClassName("botoesCarousel");
+
+let idx = 0;
+let qtd_imgs = 0;
+
+$(document).ready(function(){
+	$(".projects").click(function(e){
+		e.stopPropagation();
+		initCarousel($(this).data("imgs"));
+	});
+
+	$(".leftCarousel").click(function(e){
+		e.stopPropagation();
+		if($(this).css("opacity") > 0)
+		{
+			if(idx == 0)
+				idx = qtd_imgs-1;
+			else
+				idx--;
+
+			$(".rightCarousel").css('opacity', 1).removeClass('noCursor');
+			imgs.style.transform = `translateX(${-idx * 640}px)`;
+			botoesCarousel[0].style.transform = `translateX(${idx * 640}px)`;
+		}
+	});
+
+	$(".rightCarousel").click(function(e){
+		e.stopPropagation();
+		if($(this).css("opacity") > 0)
+		{
+			if(idx == qtd_imgs -1)
+				idx = 0;
+			else
+				idx++;
+			
+			
+			$(".leftCarousel").css('opacity', 1).removeClass('noCursor');
+			imgs.style.transform = `translateX(${-idx * 640}px)`;
+			botoesCarousel[0].style.transform = `translateX(${idx * 640}px)`;
+		}
+	});
+
+
+	$("body").click(function(e){
+		if(!$(e.target).hasClass("botoesCarousel") && 
+			!$(e.target).hasClass("rightCarousel") && 
+			!$(e.target).hasClass("leftCarousel") && 
+			$(".carousel").is(":visible"))
+		{
+			$(".carousel").fadeOut();
+		}
+	});
+
+	function initCarousel(imgs){
+		$(".carousel img").remove();
+		var imgs = imgs.split(',');
+		qtd_imgs = imgs.length;
+		idx = 0;
+
+		for(var i = 0; i < qtd_imgs; i++){
+			$(".containerCarousel").append('<img src="images/' + imgs[i] + '">');
+		}
+		
+		$(".carousel").fadeIn();
+	}
+});
